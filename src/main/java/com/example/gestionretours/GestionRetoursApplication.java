6ae28b6d779ebcab52596a6e35c9092867bc698e
LaithMahdi <mahdilaith380@gmail.com>
2026-03-17 11:44:08 +0100

    first commit

---
+++ src/main/java/com/example/gestionretours/GestionRetoursApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionretours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRetoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRetoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestionretours/GestionRetoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionretours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRetoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
